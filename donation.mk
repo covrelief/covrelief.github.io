9f6d6026df772944cb6d329e8addb75b79bb94a0
Guannan Wang <snailium@hotmail.com>
2020-02-07 01:59:51 -0500

    Added donation page.

---
+++ donation.mk
@@ -0,0 +1,23 @@
+# How to donate
+
+Donation can be made by Interac e-Transfer through all major Canadian banks. Please send your donation to donation@covrelief.ca.
+
+We may issue receipt for any donation transaction larger than $10 amount.
+
+To issue an official donation receipt for tax purposes, the following additional information is needed, as per [CRA requirement][1].
+
+- Full name, including middle initial
+- Home address
+
+Please send above information with your transaction reference number to donation@covrelief.ca
+
+All donations will be published below. All expense will be published on "Expense" page.
+
+
+# Donation list
+
+Donation list is updated on weekly basis.
+
+To be updated...
+
+[1]: https://www.canada.ca/en/revenue-agency/services/charities-giving/charities/operating-a-registered-charity/issuing-receipts/what-information-must-on-official-donation-receipt-a-registered-charity.html
